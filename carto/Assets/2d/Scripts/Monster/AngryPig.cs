87f8d650ec18a9c75eaf241801df9e286192ec4d
Kim Jinseob <seop1284@naver.com>
2022-10-05 23:09:11 +0900

    몬스터 스크립트 수정

--- carto/Assets/2d/Scripts/Monster/AngryPig.cs
+++ carto/Assets/2d/Scripts/Monster/AngryPig.cs
@@ -12,12 +12,12 @@ public class AngryPig : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Animator anim;
-    
+
     private Vector3 initialPos;
-    private bool isTargetInRange = false;
-    private bool isTargetInAttackRange = false;
+
+    private Action action = Action.IDLE;
     private bool isInAction = false;
-    
+
     private float maxRange = 4.0f;
     private float attackRange = 1.0f;
     private float moveSpeed = 1.0f;
@@ -55,80 +55,90 @@ public class AngryPig : MonoBehaviour
 
     void FixedUpdate()
     {
-        CheckTargetInRange();
-
-        // Attack
-        if (isTargetInAttackRange)
-        {
-            isInAction = true;
-            anim.SetBool("isAttacking", true);
-            StartCoroutine(CheckAnimationState());
-        }
-        else
-        {
-            anim.SetBool("isAttacking", false);
-        }
-    
+        
         if (!isInAction)
         {
-            // Moving
-            if (isTargetInRange)
+            CheckAction();
+        
+            switch (action)
             {
-                anim.SetBool("isMoving", true);
-                sr.flipX = (transform.position - target.position).x <= 0;
-                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+                case Action.MOVE2ORIGIN:
+                    anim.SetBool("isMoving", true);
+                    sr.flipX = (transform.position - initialPos).x <= 0;
+                    transform.position = Vector2.MoveTowards(transform.position, initialPos, moveSpeed * Time.fixedDeltaTime);
+                    break;
+
+                case Action.MOVE2TARGET:
+                    anim.SetBool("isMoving", true);
+                    sr.flipX = (transform.position - target.position).x <= 0;
+                    transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+                    break;
+
+                // case Action.ATTACK:
+                //     anim.SetTrigger("isAttacking");
+                //     StartCoroutine(CheckAnimationState("Attack"));
+                //     break;
+
+                case Action.IDLE:
+                    anim.SetBool("isMoving", false);
+                    sr.flipX = false;
+                    break;
+
+                default:
+                    break;
             }
-            else if (!isTargetInRange && (transform.position != initialPos))
-            {
-                anim.SetBool("isMoving", true);
-                sr.flipX = (transform.position - initialPos).x <= 0;
-                transform.position = Vector2.MoveTowards(transform.position, initialPos, moveSpeed * Time.fixedDeltaTime);
-            }
-            else
-            {
-                anim.SetBool("isMoving", false);
-                sr.flipX = false;
-            }
-        }   
+        }
     }
 
-    void CheckTargetInRange()
+    void CheckAction()
     {
-        float areaDistance = Vector2.Distance(initialPos, target.position);
-        float attackDistance = Vector2.Distance(transform.position, target.position);
+        float orgin2target = Vector2.Distance(initialPos, target.position);
+        float object2target = Vector2.Distance(transform.position, target.position);
 
-        if (areaDistance < maxRange)
+        if (orgin2target <= maxRange)
         {
-            isTargetInRange = true;
+            if (object2target <= attackRange)
+            {
+                action = Action.ATTACK;
+                return;
+            }
+
+            action = Action.MOVE2TARGET;
+            return;
+        }
+        else if (transform.position != initialPos)
+        {
+            action = Action.MOVE2ORIGIN;
+            return;
         }
         else
         {
-            isTargetInRange = false;
+            action = Action.IDLE;
+            return;
         }
 
-        if (attackDistance < attackRange)
-        {
-            isTargetInAttackRange = true;
-        }
-        else
-        {
-            isTargetInAttackRange = false;
-        }
     }
 
-    IEnumerator CheckAnimationState()
+    IEnumerator CheckAnimationState(string actionName)
     {
         
+        while (!anim.GetCurrentAnimatorStateInfo(0)
+        .IsName(actionName)) 
+        { 
+            //전환 중일 때 실행되는 부분
+            yield return null;
+        }
+
         while (anim.GetCurrentAnimatorStateInfo(0)
         .normalizedTime < 1.0f)
         {
-            //애니메이션 재생 중 실행되는 부분            
+            //애니메이션 재생 중 실행되는 부분
+            isInAction = true;         
             yield return null;
         }
         
         //애니메이션 완료 후 실행되는 부분
         isInAction = false;
     }
-
 
 }
--- carto/Assets/2d/Scripts/Monster/Chameleon.cs
+++ carto/Assets/2d/Scripts/Monster/Chameleon.cs
@@ -13,10 +13,10 @@ public class Chameleon : MonoBehaviour
     private Animator anim;
 
     private Vector3 initialPos;
-    private bool isTargetInRange = false;
-    private bool isTargetInAttackRange = false;
+
+    private Action action = Action.IDLE;
     private bool isInAction = false;
-    
+
     private float maxRange = 4.0f;
     private float attackRange = 1.0f;
     private float moveSpeed = 1.0f;
@@ -25,6 +25,18 @@ public class Chameleon : MonoBehaviour
     // * target variable
     // *****************************************************
     private Transform target;
+
+    // *****************************************************
+    // * Action Enum class
+    // *****************************************************
+    enum Action
+    {
+        IDLE,
+        MOVE2TARGET,
+        MOVE2ORIGIN,
+        ATTACK
+        
+    }
 
     void Start()
     {
@@ -54,74 +66,85 @@ public class Chameleon : MonoBehaviour
 
     void FixedUpdate()
     {
-        CheckTargetInRange();
-
-        // Attack
-        if (isTargetInAttackRange)
-        {
-            isInAction = true;
-            anim.SetBool("isAttacking", true);
-            StartCoroutine(CheckAnimationState());
-        }
-        else
-        {
-            anim.SetBool("isAttacking", false);
-        }
-    
+        
         if (!isInAction)
         {
-            // Moving
-            if (isTargetInRange)
+            CheckAction();
+        
+            switch (action)
             {
-                anim.SetBool("isMoving", true);
-                sr.flipX = (transform.position - target.position).x <= 0;
-                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+                case Action.MOVE2ORIGIN:
+                    anim.SetBool("isMoving", true);
+                    sr.flipX = (transform.position - initialPos).x <= 0;
+                    transform.position = Vector2.MoveTowards(transform.position, initialPos, moveSpeed * Time.fixedDeltaTime);
+                    break;
+
+                case Action.MOVE2TARGET:
+                    anim.SetBool("isMoving", true);
+                    sr.flipX = (transform.position - target.position).x <= 0;
+                    transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+                    break;
+
+                case Action.ATTACK:
+                    anim.SetTrigger("isAttacking");
+                    StartCoroutine(CheckAnimationState("Attack"));
+                    break;
+
+                case Action.IDLE:
+                    anim.SetBool("isMoving", false);
+                    sr.flipX = false;
+                    break;
+
+                default:
+                    break;
             }
-            else if (!isTargetInRange && (transform.position != initialPos))
-            {
-                anim.SetBool("isMoving", true);
-                sr.flipX = (transform.position - initialPos).x <= 0;
-                transform.position = Vector2.MoveTowards(transform.position, initialPos, moveSpeed * Time.fixedDeltaTime);
-            }
-            else
-            {
-                anim.SetBool("isMoving", false);
-                sr.flipX = false;
-            }
-        }   
+        }
     }
 
-    void CheckTargetInRange()
+    void CheckAction()
     {
-        float areaDistance = Vector2.Distance(initialPos, target.position);
-        float attackDistance = Vector2.Distance(transform.position, target.position);
+        float orgin2target = Vector2.Distance(initialPos, target.position);
+        float object2target = Vector2.Distance(transform.position, target.position);
 
-        if (areaDistance < maxRange)
+        if (orgin2target <= maxRange)
         {
-            isTargetInRange = true;
+            if (object2target <= attackRange)
+            {
+                action = Action.ATTACK;
+                return;
+            }
+
+            action = Action.MOVE2TARGET;
+            return;
+        }
+        else if (transform.position != initialPos)
+        {
+            action = Action.MOVE2ORIGIN;
+            return;
         }
         else
         {
-            isTargetInRange = false;
+            action = Action.IDLE;
+            return;
         }
 
-        if (attackDistance < attackRange)
-        {
-            isTargetInAttackRange = true;
-        }
-        else
-        {
-            isTargetInAttackRange = false;
-        }
     }
 
-    IEnumerator CheckAnimationState()
+    IEnumerator CheckAnimationState(string actionName)
     {
         
+        while (!anim.GetCurrentAnimatorStateInfo(0)
+        .IsName(actionName)) 
+        { 
+            //전환 중일 때 실행되는 부분
+            yield return null;
+        }
+
         while (anim.GetCurrentAnimatorStateInfo(0)
         .normalizedTime < 1.0f)
         {
-            //애니메이션 재생 중 실행되는 부분            
+            //애니메이션 재생 중 실행되는 부분
+            isInAction = true;         
             yield return null;
         }
         
