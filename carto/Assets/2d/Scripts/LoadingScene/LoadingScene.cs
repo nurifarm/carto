547a9beac9f055f6038db90ffbc5a1276452ff98
Kim Jinseob <seop1284@naver.com>
2022-10-17 23:16:28 +0900

    로딩씬

--- carto/Assets/2d/Scripts/LoadingScene/LoadingScene.cs
+++ carto/Assets/2d/Scripts/LoadingScene/LoadingScene.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using TMPro;
 using Cysharp.Threading.Tasks;
 
 public class LoadingScene : SceneBase
@@ -18,7 +14,9 @@ public class LoadingScene : SceneBase
 
     public async UniTaskVoid Load()
 	{
-        await UniTask.Delay(10000);
+        // dummy loading..
+        await UniTask.Delay(4000);
+        // Load Scene
         await CSceneManager.Instance.Change();
 	}
 }
